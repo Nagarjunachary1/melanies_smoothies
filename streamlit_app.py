c8457dc7474249f30fd1790103b830609d1ee11f
Nagarjunachary1 <77378107+Nagarjunachary1@users.noreply.github.com>
2025-07-10 01:15:19 +0530

    Update streamlit_app.py
    minor fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,8 +12,39 @@ st.write(
   """
 )
 
+name_on_order = st.text_input("Name on smoothie");
+st.write("Order Name="+name_on_order)
 
-    
+cnx =st.connection("snowflake")
+session =cnx.session()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+#st.dataframe(data=my_dataframe, use_container_width=True)
+
+ingerdient_list =st.multiselect(
+    "choose up to 6 ingerdients",
+    my_dataframe,
+    max_selections=6
+);
+
+if ingerdient_list:
+    st.write("You selected:", ingerdient_list)
+    st.text( ingerdient_list)
+
+    ingredients_string = '';
+    for fruit_chosen in ingerdient_list:
+        ingredients_string+=fruit_chosen+' ';
+        
+    st.write("ingerdient_string="+ingredients_string);
+
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+            values ('""" + ingredients_string + """','""" + name_on_order + """')"""
+    st.write(my_insert_stmt)
+
+    time_to_insert = st.button('Submit')
+
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success('Your Smoothie is ordered!', icon="✅")    
 
 
 
