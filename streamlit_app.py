c6f67e4cfa6530f6038129693d568f0cca0128c0
nagarjuna-mainACNT <nagarjuna.caesium@gmail.com>
2025-07-10 02:06:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,7 +50,7 @@ if ingerdient_list:
 
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
-sf_df=st.dataframe(smoothiefroot_response.json(),use_container_width=True)
+#st.text(smoothiefroot_response.json())
+sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
 
 
